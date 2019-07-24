6783b2b1f1e4343559bb8ddbf8cfe9e1f515071b
Karen Alvarado <karen.alvarado@one.verizon.com>
2019-07-24 10:02:53 -0400

    three

--- js/script.js
+++ js/script.js
@@ -1,30 +1,25 @@
-// $.ajax({
-//     url: 'https://randomuser.me/api/?results=12&nat=US',
-//     dataType: 'json',
-//     success: function (data) {
-//         console.log(data);
-//     }
-// });
-
+//url to get employee data 
+//created a query string to display 12 employee array and their nationality
 fetch('https://randomuser.me/api/?results=12&nat=US')
 
-    //transforms the data into json 
+    //response sends out promise and response with json data
     .then(response => response.json())
     //execute the function
     .then(function (users) {
-        // variable for user results
-        data = users.results
-        // loops through result of each array items named employees
+        // set variable for user results from the Random Generated User API's document
+        data = users.results;
+        // loops through each employee to get their information
         data.forEach(employees => {
-            // create variables for each employee information which will loop through
-            let image = employees.picture.large
-            let fName = employees.name.first
-            let lName = employees.name.last
-            let email = employees.email
-            let city = employees.location.city
-            let state = employees.location.state
+            // create variables for each employee information
+            let image = employees.picture.large;
+            let firstName = employees.name.first;
+            let lastName = employees.name.last;
+            let email = employees.email;
+            let city = employees.location.city;
+            let state = employees.location.state;
 
             //create variable for employee profile and set it to the div card
+            //dynamically adds HTML elements
             //template literal displays images, email, name, city and state on employee directory
             const profile = `
             <div class="card">
@@ -32,52 +27,63 @@ fetch('https://randomuser.me/api/?results=12&nat=US')
                     <img class="card-img" src="${image}" alt="profile picture">
                     </div>
                     <div class="card-info-container">
-                        <h3 id="name" class="card-name cap">${fName} ${lName}</h3>
+                        <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
                         <p class="card-text">${email}</p>
                         <p class="card-text cap">${city}, ${state}</p>
                     </div>
                 </div>`
-            // append profile to gallery
-            $("#gallery").append(profile)
+            // append profile to gallery id
+            $("#gallery").append(profile);
         })
     })
 
+//create variable for dditional detailed information which will appear when employee is selected
 function display(i) {
-    let image = data[i].picture.large
-    let fName = data[i].name.first
-    let lName = data[i].name.last
-    let email = data[i].email
-    let city = data[i].location.city
-    let state = data[i].location.state
-    let phone = data[i].phone
-    let street = data[i].location.street
-    let birthday = data[i].dob.date
 
+    //assigning variables globally
+    let image = data[i].picture.large;
+    let firstName = data[i].name.first;
+    let lastName = data[i].name.last;
+    let email = data[i].email;
+    let city = data[i].location.city.toUpperCase();
+    let state = data[i].location.state.toUpperCase();
 
+    //additional information
+    let phone = data[i].phone;
+    let street = data[i].location.street.toUpperCase();
+    let birthMonth = data[i].dob.date.slice(5, 7);
+    let birthDate = data[i].dob.date.slice(8, 10);
+    let birthYear = data[i].dob.date.slice(0, 4);
+
+    //adds html element of information to the pop up window
+    //layout of the card
     const modal = `
             <div class="modal-container">
                 <div class="modal">
                 <button type="button" id ="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                 <div class="modal-info-container">
                     <img class ="modal-img" src="${image}" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${fName} ${lName}</h3> 
+                    <h3 id="name" class="modal-name cap">${firstName} ${lastName}</h3> 
                     <p class="modal-text">${email}</p> 
                     <p class="modal-text cap">${city}</p> 
                     <hr>
                     <p class="modal-text">${phone}</p> 
-                    <p class="modal-text">${street}, ${city} ${state} 97204</p> 
-                    <p class="modal-text">Birthday: ${birthday}</p> 
+                    <p class="modal-text">${street}, ${city}, ${state} 97204</p> 
+                    <p class="modal-text">Birthday: ${birthMonth}/${birthDate}/${birthYear}</p> 
                 </div> 
             </div>`
-    $("body").append(modal)
+
+    //appends html to pop up window
+    $("body").append(modal);
+
+    //closes pop up window when ‘x' clicked
     $("#modal-close-btn").on("click", function () {
-        $(".modal-container").remove()
+        $(".modal-container").remove();
     });
-
 }
-document.getElementById('gallery').addEventListener("click", function () {
+
+//when clicked on employee card, modal container appears
+$('#gallery').on("click", ".card", function () {
     i = ($(this).index())
-    display(i)
-
-
+    display(i);
 });
